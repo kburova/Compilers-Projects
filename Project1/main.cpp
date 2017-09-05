2c7bc9036637b662e9580fa6c05e2db0e8515701
Ksenia Burova <kburova@vols.utk.edu>
2017-09-04 22:21:59 -0400

    Created 1st project

---
+++ Project1/main.cpp
@@ -0,0 +1,23 @@
+/*****************************************
+ * CS 461. Project 1.
+ * Completed by : Ksenia Burova
+ *
+ * Implementation of NFA to DFA algorithm
+ *****************************************/
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string line;
+    while(getline(cin,line)){
+        cout << line << endl;
+    }
+
+    return 0;
+}
